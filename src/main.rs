c3769ef72bbd6202feab640862ecf6ccbaf74935
Tanat <ttangun1@gmail.com>
2023-04-04 14:55:00 +0700

    feat: add interval implementation for ohlc endpoint
    change the order to ascending

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use db_updater::Ohlc;
 use futures::stream::TryStreamExt;
-use mongodb::bson::doc;
-use mongodb::options::FindOptions;
+use mongodb::bson::{doc, Document};
+use mongodb::Collection;
 use rocket::serde::json::Json;
-use rocket::{get, launch, routes};
+use rocket::{get, launch, routes, FromFormField};
+use rocket_cors::{CorsOptions, AllowedOrigins, Cors};
 use rocket_db_pools::{mongodb, Connection, Database};
 
 // we need to specify the database url on Rocket.toml like this
@@ -13,21 +14,93 @@ use rocket_db_pools::{mongodb, Connection, Database};
 #[database("marketdata")]
 struct MarketData(mongodb::Client);
 
-#[get("/ohlc?<symbol>")]
-async fn ohlc(db: Connection<MarketData>, symbol: &str) -> Json<Vec<Ohlc>> {
+#[derive(Debug, PartialEq, FromFormField)]
+enum Interval {
+    #[field(value = "1h")]
+    OneHour,
+    #[field(value = "4h")]
+    FourHour,
+    #[field(value = "1d")]
+    OneDay,
+}
+
+fn aggrdoc_to_ohlc(docs: Vec<Document>) -> Vec<Ohlc> {
+    docs.iter()
+        .map(|x| Ohlc {
+            ticker: x.get_document("_id").unwrap().get_str("ticker").unwrap().to_string(),
+            time: x
+                .get_document("_id")
+                .unwrap()
+                .get_datetime("time")
+                .unwrap()
+                .clone(),
+            open: x.get_f64("open").unwrap(),
+            close: x.get_f64("close").unwrap(),
+            high: x.get_f64("high").unwrap(),
+            low: x.get_f64("low").unwrap(),
+            volume: x.get_i64("volume").unwrap() as u64,
+        })
+        .collect()
+}
+
+async fn aggr_fetch(collection: &Collection<Ohlc>, interval: Option<Interval>) -> Vec<Ohlc> {
+    let result = collection
+        .aggregate(
+            vec![
+                doc! {"$group" : {
+                    "_id" : {
+                        "ticker": "$ticker",
+                        "time": {
+                            "$dateTrunc" : {
+                                "date": "$time",
+                                "unit": "hour",
+                                "binSize": match interval {
+                                    Some(Interval::OneHour) => 1,
+                                    Some(Interval::FourHour) => 4,
+                                    Some(Interval::OneDay) => 24,
+                                    None => 1,
+                                }
+                            }
+                        }
+                    },
+                    "open": {"$first": "$open"},
+                    "close": {"$last": "$close"},
+                    "high": {"$max": "$high"},
+                    "low": {"$min": "$low"},
+                    "volume": {"$sum": "$volume"},
+                }},
+                doc! {"$sort": {"_id.time": 1}},
+            ],
+            None,
+        )
+        .await
+        .unwrap();
+
+    aggrdoc_to_ohlc(result.try_collect::<Vec<Document>>().await.unwrap())
+}
+
+#[get("/ohlc?<symbol>&<interval>")]
+async fn ohlc(
+    db: Connection<MarketData>,
+    symbol: &str,
+    interval: Option<Interval>,
+) -> Json<Vec<Ohlc>> {
     let marketdata = &*db;
     let db = marketdata.database("StockMarket");
     let collection = db.collection::<Ohlc>(symbol);
 
-    let find_options = FindOptions::builder().sort(doc! {"time": -1}).build();
-    let result = collection.find(None, find_options).await.unwrap();
-    let data = result.try_collect::<Vec<Ohlc>>().await.unwrap();
-    Json(data)
+    Json(aggr_fetch(&collection, interval).await)
 }
 
 #[launch]
 fn rocket() -> _ {
+    // Configure CORS options
+    let cors_option = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::All);         
+    let cors = Cors::from_options(&cors_option).unwrap();
+
     rocket::build()
+        .attach(cors)
         .attach(MarketData::init())
         .mount("/api", routes![ohlc])
 }
