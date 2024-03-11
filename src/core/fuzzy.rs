baa3649587433f0d5a910e49ce0dcc00d07f27e6
Tanat <ttangun1@gmail.com>
2024-03-11 15:08:14 +0700

    fix: bb_percent bug

--- src/core/fuzzy.rs
+++ src/core/fuzzy.rs
@@ -62,14 +62,14 @@ pub fn create_fuzzy_engine(
     fuzzy_engine
 }
 
-fn bb_percent(price: f64, v: (f64, f64, f64)) -> f64 {
+pub fn bb_percent(price: f64, v: (f64, f64, f64)) -> f64 {
     let (sma, lower, upper) = v;
 
     if price > sma {
-        return (price - sma) / (upper - sma);
+        return (price - sma) / (upper - sma) * 100.0;
     }
 
-    (sma - price) / (sma - lower)
+    (sma - price) / (sma - lower) * 100.0
 }
 
 /// Compares the current value of data to it's value `length` bars ago and return the normalized
